635e6afdc11bca6b2b9dd3fdaa76224188f580aa
doctorOb <drob.drobertson@gmail.com>
2014-08-27 08:17:34 -0400

    added utils class to tickr_dba

--- mysql_user_setup.sh
+++
@@ -1,5 +0,0 @@
-#set up mysql server to run as logged in user
-unset TMPDIR
-mysql_install_db --verbose --user=`whoami` \\
---basedir="$(brew --prefix mysql)" \\
---datadir=/usr/local/var/mysql --tmpdir=/tmp
